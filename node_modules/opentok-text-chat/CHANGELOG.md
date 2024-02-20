a1d905f90be4e1abdeadb6f7e87e4929e69c2e2d
CsAlkemy <alkemy@frontpayment.no>
2024-02-20 15:42:10 +0600

    without keys

---
+++ node_modules/opentok-text-chat/CHANGELOG.md
@@ -0,0 +1,38 @@
+# Accelerator TextChat js CHANGELOG
+
+All notable changes to this project will be documented in this file.
+
+--------------------------------------
+#### [1.0.31]
+
+[UPDATED] `appendControl` now allows controls to display when `alwaysOpen` is true
+
+[UPDATED] Updated all dependencies
+
+[UPDATED] Fix xss vulnerabilities
+
+#### [1.0.26]
+
+[UPDATED] Internal client log version
+
+#### [1.0.25]
+
+[UPDATED] Package info
+
+#### [1.0.24]
+
+[ADDED] Internal client logging
+
+#### [1.0.23]
+
+[ADDED]	AppendControl option
+
+[ADDED]	DeliverUnsentMessages callback
+
+[ADDED]	opentok-solutions-css for styling
+
+[ADDED]	WaitingMessage option
+
+#### [1.0.0]
+
+Official release
---
+++ node_modules/opentok-text-chat/karma.conf.js
@@ -0,0 +1,57 @@
+module.exports = function(config) {
+    var customLaunchers = {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    };
+
+    var configuration = {
+        plugins: [
+          'karma-mocha', 'karma-coverage', 'karma-html2js-preprocessor', 'karma-chrome-launcher', 'karma-chai'
+        ],
+        basePath: '',
+        frameworks: ['mocha', 'chai'],
+        client: {
+          contextFile: '/test/index.html'
+        },
+        files: [
+          'https://static.opentok.com/v2/js/opentok.min.js',
+          'https://code.jquery.com/jquery-1.10.2.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js',
+          'node_modules/opentok-solutions-logging/src/opentok-solutions-logging.js',
+          'test/opentok-text-chat-test.js',
+          'src/opentok-text-chat.js'
+        ],
+        exclude: [
+        ],
+        preprocessors: {
+            'test/*.html': ['html2js'],
+            'src/*.js': ['coverage']
+        },
+        reporters: ['progress', 'coverage', 'dots'],
+        port: 9877,
+        colors: true,
+        autoWatch: true,
+        browsers: [],
+        singleRun: true,
+        logLevel: config.LOG_INFO,
+        coverageReporter: {
+            dir: 'test/coverage',
+            instrumenter: {
+                'src/*.js': ['istanbul']
+            },
+            reporters: [
+                { type: 'html', subdir: 'report-html' },
+                { type: 'lcov', subdir: 'report-lcov' },
+                { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' }
+            ]
+        },
+    };
+
+    if (process.env.TRAVIS) {
+      configuration.customLaunchers = customLaunchers;
+      configuration.browsers = Object.keys(customLaunchers);
+    }
+    config.set(configuration);
+};
